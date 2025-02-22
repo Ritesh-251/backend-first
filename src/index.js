8a25258ede156818ffa6f19a8babed4adcd6d054
Ritesh-251 <hareradheharekrishna23@gmail.com>
2025-02-22 20:09:49 +0530

    connected to database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import dotenv from "dotenv";
+import {DB_NAME} from "./constants.js"
+dotenv.config({ path: "./.env" });
+
+import connectDB from "./db/index.js";
+connectDB();
+      
 console.log("radha radha");
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
+console.log("DB_NAME:", DB_NAME);
