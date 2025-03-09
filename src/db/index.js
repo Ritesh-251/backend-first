5783c0b0045a7338c06e7ffb4837c5059af99161
Ritesh-251 <hareradheharekrishna23@gmail.com>
2025-03-09 16:21:51 +0530

    understood the util code

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,4 @@
 import mongoose from 'mongoose'
 import {DB_NAME} from "../constants.js"
-function connectDB(){ return mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`).then(
-    () => {
-        console.log("Connected to MongoDB successfully DB HOST :");
-      }).catch((error) => {
-        console.error("ERROR:", error);
-        process.exit(1);
-      })};
+function connectDB(){ return mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)};
 export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import dotenv from "dotenv";
 import {DB_NAME} from "./constants.js"
 dotenv.config({ path: "./.env" });
-
+import {app} from "./app.js"
 import connectDB from "./db/index.js";
-connectDB();
+
+connectDB()
+  .then(() => {
+    console.log("Connected to MongoDB successfully DB HOST:");
+    app.listen( 8000, () => {
+      console.log(`Server is running on port ${8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("ERROR:", error);
+    process.exit(1); 
+  });
+
+
       
 console.log("radha radha");
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
