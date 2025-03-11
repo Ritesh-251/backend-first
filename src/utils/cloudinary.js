5cf2c527808206bacc7e49f8e0ee53ca03453585
Ritesh-251 <hareradheharekrishna23@gmail.com>
2025-03-11 23:39:39 +0530

    added functionality for file upload

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,23 @@
 import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
 
     cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_SECRET, 
-        api_secret: CLOUDINARY_CLOUD_NAME // Click 'View API Keys' above to copy your API secret
+        api_secret: process.env.CLOUDINARY_CLOUD_NAME 
     });
     
+    const uploadOnCloudinary = async function (localfilePath){
+        try{
+        if(!localfilePath) return null;
+        const response = await cloudinary.uploader.upload(localfilePath,{
+            resource_type:"auto"
+        })
+        console.log("file has been uploaded",response.url);
+    }catch(error){
+        fs.unlinkSync(localfilePath);
+        return null;
+
+
+    }
+}
